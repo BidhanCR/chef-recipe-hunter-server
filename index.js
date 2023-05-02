3927a750f4d6d12353e5e1e10c291c2ea4805122
BidhanCR <bidhanroy300805@gmail.com>
2023-05-02 22:20:17 +0600

    best-recipes data created

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-
+const chefs = require("./data/chefs.json");
 
 app.use(cors());
 
-app.get('/', (req, res)=>{
-    res.send("FoodieFrenzy is running")
-})
+app.get("/", (req, res) => {
+  res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const chef = chefs.find((n) => n.id === id);
+  res.send(chef);
+});
 
 app.listen(port, () => {
-    console.log(`FoodieFrenzy is running on port: ${port}`)
-})
+  console.log(`FoodieFrenzy is running on port: ${port}`);
+});
